29444b90f0949c335b39a60e9e70f7f1ad4eb07e
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-08 19:50:46 +0530

    Speed and Performance

--- leads_section/views.py
+++ leads_section/views.py
@@ -23,8 +23,6 @@ from rest_framework.permissions import IsAuthenticated
 from django.db.models import Count, Q
 from django.db.models.functions import TruncMonth
 from django.utils.timezone import localtime
-from django.db.models import Count, Q, Case, When, Value, CharField
-from django.core.cache import cache
 
 # Create your views here.
 import logging
@@ -397,17 +395,13 @@ def leads_graph_data(request):
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_new_leads(request):
-    cached_data = cache.get('new_leads')
-    if cached_data:
-        return Response(cached_data, status=200)
-
-    leads = Leads.objects.filter(stage="Not Opened", status="Pending") \
-                         .only('id', 'name', 'email', 'timestamp', 'stage', 'status') \
-                         .order_by('-timestamp')[:100]
-    data = list(leads.values('id', 'name', 'email', 'timestamp', 'stage', 'status'))
-
-    cache.set('new_leads', data, timeout=30)  # Cache for 30 seconds
-    return Response(data, status=200)
+    
+    leads = Leads.objects.filter(stage="Not Opened",status="Pending").order_by('-timestamp')
+    
+    # Serialize leads data
+    serializer = LeadsViewSerializer(leads, many=True)
+    
+    return Response(serializer.data, status=200)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated,IsCustomAdminUser])
