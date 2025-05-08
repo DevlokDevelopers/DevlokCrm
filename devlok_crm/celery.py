adaa47e76ed06aa0a7a53e3b7471b4117f57381a
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-08 21:28:04 +0530

    Speed and Performance

--- devlok_crm/celery.py
+++ devlok_crm/celery.py
@@ -11,3 +11,5 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Auto-discover tasks from installed apps
 app.autodiscover_tasks()
+
+app.conf.broker_connection_retry_on_startup = True
--- devlok_crm/urls.py
+++ devlok_crm/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
     # View to refresh JWT token
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
